955ed38ba11ced3885d71e2ca1382e012eabafea
Stefan-Aldus <9018284@student.zadkine.nl>
2022-09-15 14:58:43 +0200

    Opdracht 3.1

--- opdracht 3.1/app.js
+++ opdracht 3.1/app.js
@@ -8,3 +8,16 @@ document.write(myAge);
 document.write(diploma);
 document.write(passed);
 
+let num1 = 10;
+let num2 = 20;
+let answer = num1 + num2;
+console.log(answer);
+
+let answer2 = num1 - num2;
+console.log(answer2);
+
+let answer3 = num1 * num2;
+console.log(answer3);
+
+let answer4 = num1 / num2;
+console.log(answer4);
