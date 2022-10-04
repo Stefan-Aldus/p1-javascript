64b1d92d77d42014e0b4c0a13591fd6ce61c9d8b
Stefan-Aldus <9018284@student.zadkine.nl>
2022-10-04 11:29:15 +0200

    Opdracht 6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,29 @@
+function calculate(num, num2) {
+  answer = num + num2;
+  console.log(answer);
+}
+
+calculate(1, 12);
+
+function multiplybyfive(num3) {
+  answer = num3 * 5;
+  console.log(answer);
+}
+
+multiplybyfive(10);
+
+function checkInput(num4, num5) {
+  if (num4 >= num5) {
+    console.log(num4);
+  } else {
+    console.log(num5);
+  }
+}
+checkInput(9, 20);
+
+function calculateMinutes(seconds) {
+  answer = seconds / 60;
+  console.log(answer);
+}
+
+calculateMinutes(5749);
